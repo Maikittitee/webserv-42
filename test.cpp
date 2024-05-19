f48fca436e590bc2bc6f57dd7cc7431c47f83d53
Nawaporn Kietweerachon <nkietwee@z1t3c1.42bangkok.com>
2024-05-19 20:18:09 +0700

    add locataion_2

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 /*   By: nkietwee <nkietwee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 02:07:16 by nkietwee          #+#    #+#             */
-/*   Updated: 2024/05/13 16:35:28 by nkietwee         ###   ########.fr       */
+/*   Updated: 2024/05/19 20:18:07 by nkietwee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 
 std::string trim(const std::string& str)
 {
-  // Find the first non-space character from the beginning
 	size_t start = str.find_first_not_of(" ");
 	std::cout << "start : " << start << std::endl;
-  // Find the last non-space character from the end (excluding null terminator)
 	size_t end = str.find_last_not_of(" ") + 1;
 	std::cout << "end : " << end << std::endl;
 
