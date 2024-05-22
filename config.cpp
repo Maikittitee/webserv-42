aad870c984ba7e7f33f903e0df5f41df0d335f12
Nawaporn Kietweerachon <nkietwee@z3t11c2.42bangkok.com>
2024-05-22 14:30:20 +0700

    update getkey

--- config.cpp
+++ config.cpp
@@ -6,88 +6,13 @@
 /*   By: nkietwee <nkietwee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 02:07:08 by nkietwee          #+#    #+#             */
-/*   Updated: 2024/05/19 20:15:24 by nkietwee         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:28:39 by nkietwee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
 #include <cstring>
-
-bool	ft_check_extension(char	*file)
-{
-	char *tmp;
-
-	tmp = strrchr(file, '.');
-	if (strcmp(tmp, ".conf") == 0)
-		return (true);
-	return (false);
-}
-std::string	ft_getvalue(std::string key, std::string sp_line)
-{
-	int			i;
-	std::string	value;
-
-	i = 0;
-	while (sp_line[i])
-	{
-		
-		while (sp_line[i] == key[i])
-			i++;
-		while (sp_line[i] && isspace(sp_line[i]))
-			i++;
-		while (sp_line[i] != ';' && sp_line[i])
-		{
-			value += sp_line[i];
-			// value.append(sp_line[i]);
-			i++;
-		}
-	}
-	return (value);
-}
-// std::string	ft_getvalue(std::string txt)
-// {
-// 	char	*key;
-// 	char	*tmp_txt;
-	
-// 	tmp_txt = (char *)txt.c_str();	
-// 	std::cout << "--------" << std::endl;
-// 	std::cout << txt << std::endl;
-// 	std::cout << "--------" << std::endl;
-	
-// 	key = strtok((char *)txt.c_str(), " ");
-// 	while(key)
-// 	{
-// 		std::cout << key << std::endl;
-// 		key = strtok(NULL, " ");		
-// 	}
-// 	return (txt);
-// }
-
-std::string ft_getkey(std::string txt)
-{
-	int	i;
-	std::string key;
-
-	i = 0;
-	
-	while (txt[i])
-	{	
-		while (txt[i] && isspace(txt[i])) // escape isspace
-			i++;
-		while (txt[i]) // keep txt
-		{
-			key += txt[i];
-			if (txt[i] && (txt[i] == '{' || txt[i] == '}' || txt[i] == ';'))
-				break;
-			i++;
-		}
-		break;
-	}
-	std::cout << "key : " << key << std::endl;  
-	return (key);
-}
-
 
 std::string	ft_trimtab(std::string line)
 {
@@ -132,6 +57,63 @@ std::string	ft_trim(std::string line, char c)
 		return (line.substr(start, end - start));
 	return ("");
 }
+bool	ft_check_extension(char	*file)
+{
+	char *tmp;
+
+	tmp = strrchr(file, '.');
+	if (strcmp(tmp, ".conf") == 0)
+		return (true);
+	return (false);
+}
+std::string	ft_getvalue(std::string key, std::string sp_line)
+{
+	int			i;
+	std::string	value;
+
+	i = 0;
+	while (sp_line[i])
+	{
+		
+		while (sp_line[i] == key[i])
+			i++;
+		while (sp_line[i] && isspace(sp_line[i]))
+			i++;
+		while (sp_line[i] != ';' && sp_line[i])
+		{
+			value += sp_line[i];
+			// value.append(sp_line[i]);
+			i++;
+		}
+	}
+	return (value);
+}
+std::string ft_getkey(std::string txt)
+{
+	int	i;
+	std::string key;
+
+	i = 0;
+	
+	// std::cout << "ft_getkey" << std::endl;
+	while (txt[i])
+	{	
+		while (txt[i] && isspace(txt[i])) // escape isspace
+			i++;
+		while (txt[i]) // keep txt
+		{
+			if (txt[i] && (txt[i] == '{' || txt[i] == '}' || txt[i] == ';' || isspace(txt[i])))
+				break;
+			key += txt[i];
+			i++;
+		}
+		break;
+	}
+	std::cout << "|" << key << "|" << std::endl;  
+	return (key);
+}
+
+
 
 int main(int ac ,char **av)
 {
@@ -152,25 +134,16 @@ int main(int ac ,char **av)
 	while(std::getline(input_file, line)) // return integer representing the status  of read not actual content of the line
 	{
 		
-		// sp_line = ft_trimsp(line);
-		// sp_line = ft_trimtab(sp_line);
-		// sp_line = ft_trim(line, ' ');
-		// sp_line = ft_trim(sp_line, '	');
-		// std::cout << "sp_line : " << sp_line << "|" << std::endl;
-		
-		key = ft_getkey(sp_line);
+		key = ft_getkey(line);
 		// std::cout << "before value" << std::endl;
 		// value = ft_getvalue(key, sp_line);
 		// if (key.empty())
 			// std::cout << "Empty" << std::endl;
 		// else if ( key != "{" && key != "}")
-		std::cout << key << " : " << value << std::endl;
-		// std::cout << "key : " << key << "|" << std::endl;
-		// std::cout << "key : " << key << "|" << std::endl;
-		// std::cout << "value : " << value << "|" << std::endl;
-		i++;
-		if (i == 4)
-			break;
+		// std::cout << key << " : " << value << std::endl;
+		// i++;
+		// if (i == 4)
+		// 	break;
 		// std::cout << line << std::endl;
 		
 	}
