0eb2af5c4de0ed1fabe3579b01c3322293137826
ktunchar <maikittitee1615@gmail.com>
2024-06-04 18:33:59 +0700

    feat: main loop for read

--- srcs/WebServer.cpp
+++ srcs/WebServer.cpp
@@ -4,13 +4,12 @@ WebServer::WebServer(){}
 
 WebServer::~WebServer(){}
 
-bool	WebServer::_setSockAddr(struct sockaddr_in &addr, Server & serv) {
+bool	WebServer::_setSockAddr(struct sockaddr_in &addr, Server &serv) {
 	
 	bzero(&addr, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	addr.sin_port = htons(serv.port);
-
 	return true;
 }
 
@@ -85,62 +84,41 @@ bool WebServer::_set_fd(int fd, fd_set &set)
 
 bool WebServer::runServer(void)
 {
-	Client client;
-	char buffer[1024];
-
-	client.fd = accept(_servers.begin()->_server_fd, (struct sockaddr *)&client.addr, &client.addrLen);
-	std::cout << "client fd: " << client.fd << std::endl;
-
-	read(client.fd, buffer, 1024);
-
-	std::cout << buffer << std::endl;
 	
-	// fd_set tmp_read_fds;
+	fd_set tmp_read_fds;
+	char buffer[1024];
 	// fd_set tmp_write_fds;
 
-	// _init_fds();
-	// while (true)
-	// {
-	// 	tmp_read_fds = _read_fds;
-	// 	tmp_write_fds = _write_fds;
-	// 	// select 
-	// 	int status = select(_max_fd + 1, &tmp_read_fds, &tmp_write_fds, NULL, &_timeOut);
-	// 	if (status == 0)
-	// 		std::cout << "Time out" << std::endl;
-	// 	else if (status == -1){
-	// 		std::cerr << "select error " << std::endl;
-	// 		return (false);
-	// 	}
-	// 	for (int fd = 0; fd < _max_fd; fd++){
-	// 		if (FD_ISSET(fd, &_read_fds))
-	// 		{
-	// 			if (fd == _servers.begin()->_server_fd)
-	// 			{
-	// 				Client client;
-	// 				client.fd = accept(fd, (sockaddr *)&client.addr, &client.addrLen);
-	// 				_clients.insert(std::pair<int, Client>(client.fd, client));
-	// 				FD_SET(client.fd, &_read_fds);
-	// 				std::cout << "accept" << std::endl;
-	// 			}
-	// 			else
-	// 			{
-	// 				std::cout << "not match " << std::endl;
-	// 				FD_CLR(fd, &_read_fds);
-	// 			}
-	// 		}
-	// 	}
-		
-		// if time out -> 
-		// if select error ->
-
-		// loop every fd
-		// if read 
-		// 		if new connection -> accept
-		// 		else -> parsing request
-		// else if write
-		//		send response
-
-	// }
+	_init_fds();
+	while (true)
+	{
+		tmp_read_fds = _read_fds;
+		// tmp_write_fds = _write_fds;
+		// select 
+		int status = select(_max_fd + 1, &tmp_read_fds, NULL, NULL, NULL);
+		if (status == -1){
+			std::cerr << "select error " << std::endl;
+			return (false);
+		}
+		for (int fd = 0; fd <= _max_fd; fd++){
+			if (FD_ISSET(fd, &tmp_read_fds))
+			{
+				if (fd == _servers.begin()->_server_fd)
+				{
+					Client client;
+					client.fd = accept(fd, (sockaddr *)&client.addr, &client.addrLen);
+					_clients.insert(std::pair<int, Client>(client.fd, client));
+					_set_fd(client.fd, _read_fds);
+				}
+				else
+				{
+					read(fd, buffer, 1024);
+					std::cout << buffer << std::endl;
+					FD_CLR(fd, &_read_fds);
+				}
+			}
+		}
+	}
 	return (true);
 
 }
