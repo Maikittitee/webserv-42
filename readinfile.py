e00bc471757484e8f20c5f1739ad5501f4935829
ktunchar <maikittitee1615@gmail.com>
2024-06-12 02:43:18 +0700

    shit

--- readinfile.py
+++ readinfile.py
@@ -1,6 +1,7 @@
 #!/opt/homebrew/bin/python3
 import cgi
 
+
 f = cgi.FieldStorage()
 
 print("Content-type: text/html" )
@@ -8,4 +9,4 @@ print()
 
 vars = f.keys()
 for v in vars:
-    print('%s = %s<br>' % (v,f[v].value))
+    print('%s = %s<br>' %(v,f[v].value))
